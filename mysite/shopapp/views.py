ca1b22fd727a104e5bea5dcbf05ae606668053af
Вячеслав Янушкевич <cool.9nyw@mail.ru>
2023-05-30 09:03:15 +0300

    add lesson 3.3

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.models import Group
 
 
 def shop_index(request: HttpRequest):
     return render(request, 'shopapp/shop-index.html')
 
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.prefetch_related('permissions').all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+
+    }
+    return render(request, 'shopapp/products-list.html', context)
