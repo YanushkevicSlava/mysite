212b7db588f5cc6f79c7e5725ce5acf5f578ff5e
Вячеслав Янушкевич <cool.9nyw@mail.ru>
2023-06-04 13:29:57 +0300

    add fields to admin.py

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -15,7 +15,22 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = "pk", "name", "description_short", "price", "discount"
     list_display_links = "pk", "name"
     ordering = "-name", "pk"
-    search_fields = "name", "description"
+    search_fields = "name", "pk"
+    fields = [
+        (None, {
+            "fields": ("name", "description"),
+        }),
+        ("Price option", {
+            "fields": ("price", "discount"),
+            "classes": ("wide", "collapse"),
+        }),
+        ("Extra option", {
+            "fields": ("archived",),
+            "classes": ("collapse",),
+            "description": "Extra options. Field 'archived' is for soft delete",
+
+        })
+    ]
 
     def description_short(self, obj: Product) -> str:
         if len(obj.description) < 48:
@@ -33,6 +48,7 @@ class OrderAdmin(admin.ModelAdmin):
         ProductInline,
     ]
     list_display = "delivery_address", "promocode", "created_at", "user_verbose"
+    search_fields = "delivery_address", "promocode"
 
     def get_queryset(self, request):
         return Order.objects.select_related("user").prefetch_related("products")
