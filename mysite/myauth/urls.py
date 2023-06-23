5ba1db16d9516f92d9affcdbe6ca90625c45e331
Вячеслав Янушкевич <cool.9nyw@mail.ru>
2023-06-23 14:59:42 +0300

    add homework module 8

---
+++ mysite/myauth/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from .views import (
+    set_cookie_view,
+    get_cookie_view,
+    set_session_view,
+    get_session_view,
+    MyLogoutView,
+)
+from django.contrib.auth.views import LoginView
+
+
+app_name = 'myauth'
+
+urlpatterns = [
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="myauth/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login"),
+    path("logaut/", MyLogoutView.as_view(), name="logaut"),
+    path("cookie/get/", get_cookie_view, name="cookie-get"),
+    path("cookie/set/", set_cookie_view, name="cookie-set"),
+    path("session/get/", get_session_view, name="session-get"),
+    path("session/set/", set_session_view, name="session-set"),
+]
